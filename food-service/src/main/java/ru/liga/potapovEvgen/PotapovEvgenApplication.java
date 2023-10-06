758af701ed20515d2c0409aa57452dd314d080a4
EvgeniPotapov <108694988+EvgeniPotapov@users.noreply.github.com>
2023-10-06 18:13:36 +0300

    EvgrniPotapov
    The project was built using Maven

---
+++ food-service/src/main/java/ru/liga/potapovEvgen/PotapovEvgenApplication.java
@@ -0,0 +1,13 @@
+package ru.liga.foodService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotapovEvgenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotapovEvgenApplication.class, args);
+	}
+
+}
---
+++ food-service/src/test/java/ru/liga/potapovEvgen/PotapovEvgenApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.liga.potapovEvgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotapovEvgenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
